fcef167294137138f1c71437c4452fb6a558c574
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2019-01-24 08:41:33 +0100

    chore: add types

--- src/typescript/index.d.ts
+++ src/typescript/index.d.ts
@@ -1,8 +1,8 @@
 type StringValue = string | void | null;
-type verdaccio$Callback = Function;
-type verdaccio$StorageList = Array<string>;
 
 declare namespace Verdaccio {
+	type StorageList = Array<string>;
+	type Callback = Function;
 	type Author = {
 		name: string;
 		email: string;
@@ -14,13 +14,86 @@ declare namespace Verdaccio {
 		tarball: string;
 	}
 
+	type RemoteUser = {
+		real_groups: Array<string>;
+		groups: Array<string>;
+		name: string | void;
+		error?: string;
+	}
+
 	interface LocalStorage {
 		list: any;
 		secret: string;
 	}
 
+	type Version = {
+		name: string,
+		version: string,
+		devDependencies: string,
+		directories: any,
+		dist: Dist,
+		author: string,
+		main: string,
+		homemage: string,
+		license: string,
+		readme: string,
+		readmeFileName: string,
+		description: string,
+		bin: string,
+		bugs: any,
+		files: Array<string>,
+		gitHead: string,
+		maintainers: Array<Author>,
+		repository: string | any,
+		scripts: any,
+		homepage: string,
+		dist: Dist,
+		readmeFilename: string;
+		etag: string;
+		contributors: Array<Author>,
+		dependencies: any,
+		keywords: string | Array<string>,
+		_id: string,
+		nodeVersion: string,
+		_npmUser: Author,
+		_hasShrinkwrap: boolean
+	};
 
+	type Logger = {
+		child: (conf: any) => any;
+		debug: (conf: any, template?: mixed) => void;
+		error: (conf: any, template?: mixed) => void;
+		http: (conf: any, template?: mixed) => void;
+		trace: (conf: any, template?: mixed) => void;
+		warn: (conf: any, template?: mixed) => void;
+		info: (conf: any, template?: mixed) => void;
+	}
 
+	type Versions = {
+		[key: string]: Version;
+	}
+
+	type DistFile = {
+		url: string;
+		sha: string;
+		registry?: string;
+	}
+
+	type MergeTags = {
+		[key: string]: string;
+	}
+
+	type DistFiles = {
+		[key: string]: DistFile;
+	}
+
+	type AttachMents = {
+		[key: string]: Version;
+	}
+
+	type GenericBody = {
+		[key: string]: string;
+	}
 }
 
 declare module "@verdaccio/types" {
