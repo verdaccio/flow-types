59a03e75b8b4b7b1a9fff251168a7606e42d299e
Manuel Spigolon <behemoth89@gmail.com>
2019-05-13 22:18:51 +0200

    fix: revert token signature

--- index.d.ts
+++ index.d.ts
@@ -385,9 +385,6 @@ type StringValue = string | void | null;
 									transformPackage: Function,
 									onEnd: Callback): void;
 		savePackage(fileName: string, json: Package, callback: Callback): void;
-		saveToken(token: Token): Promise<any>;
-		deleteToken(user: string, tokenKey: string): Promise<any>;
-		readTokens(filter: TokenFilter): Promise<Array<Token>>;
 	}
 
 	interface TarballActions {
@@ -401,9 +398,6 @@ type StringValue = string | void | null;
 		mergeTags(name: string, tags: MergeTags, callback: Callback): void;
 		removePackage(name: string, callback: Callback): void;
 		changePackage(name: string, metadata: Package, revision: string, callback: Callback): void;
-		saveToken(token: Token): Promise<any>;
-		deleteToken(user: string, tokenKey: string): Promise<any>;
-		getTokens(filter: TokenFilter): Promise<Array<Token>>;
 	}
 
 	interface IStorageManager<T> extends StoragePackageActions {
